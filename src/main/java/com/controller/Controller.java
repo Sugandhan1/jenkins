7ec2b3d5f2c810bf961687b56c830a15d6eee3cb
Sugandhan1 <harisugandhan@gmail.com>
2026-02-18 17:15:19 +0530

    commited 2nd time

--- src/main/java/com/controller/Controller.java
+++ src/main/java/com/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +15,9 @@ public class Controller
 		return "Hello World";
 	}
 	
+	@GetMapping (value = "/getname/{name}")
+	public String getName(@PathVariable String name)
+	{
+		return name;
+	}
 }
